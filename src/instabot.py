132405561d9cd60500a75e94849b145c2d6a10f7
Greg Rafferty <greg.rafferty+git@gmail.com>
2018-08-24 13:20:04 -0700

    deleted comments

--- src/instabot.py
+++ src/instabot.py
@@ -276,23 +276,10 @@ class InstaBot:
         })
 
         r = self.s.get(self.url)
-        # self.s.headers.update({'X-CSRFToken': r.cookies['csrftoken']})
-		
-		
         csrf_token = re.search('(?<=\"csrf_token\":\")\w+', r.text).group(0)
         self.s.headers.update({'X-CSRFToken': csrf_token})
-		
-		
-        time.sleep(5 * random.random())
-		
-        # login = self.s.post(
-        #     self.url_login, data=self.login_post, allow_redirects=True)
-        # self.s.headers.update({'X-CSRFToken': login.cookies['csrftoken']})
-        # self.csrftoken = login.cookies['csrftoken']
-        #ig_vw=1536; ig_pr=1.25; ig_vh=772;  ig_or=landscape-primary;
-		
-		
-        login = self.s.post(self.url_login, data=self.login_post, allow_redirects=True)
+		time.sleep(5 * random.random())
+		login = self.s.post(self.url_login, data=self.login_post, allow_redirects=True)
         self.csrftoken = csrf_token
 		
 		
